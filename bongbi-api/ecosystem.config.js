83bf87133f1e3ec77da93f28254d74582d90b876
Juno961 <tj11629@naver.com>
2025-09-02 21:26:34 +0900

    Update ecosystem.config.js

--- bongbi-api/ecosystem.config.js
+++ bongbi-api/ecosystem.config.js
@@ -2,12 +2,10 @@ module.exports = {
   apps: [
     {
       name: "bongbi-api",
-      script: "uvicorn",
+      script: "/root/bongbi-SaaS-v2/bongbi-api/.venv/bin/uvicorn",
       args: "app.main:app --host 0.0.0.0 --port 8000",
-      interpreter: "none",   // uvicorn을 직접 실행하도록 설정
-      cwd: "/root/bongbi-SaaS-v2/bongbi-api", // 서버 내 실제 경로로 수정
+      cwd: "/root/bongbi-SaaS-v2/bongbi-api",
       env: {
-        // 환경변수는 여기서 정의
         VITE_API_BASE_URL: "/api/v1/",
         PYTHONPATH: "."
       }
