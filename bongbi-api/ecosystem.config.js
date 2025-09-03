1c04dd18769be26e3a59bcc3400985557f395076
Juno961 <tj11629@naver.com>
2025-09-03 09:42:56 +0900

    Update ecosystem.config.js

--- bongbi-api/ecosystem.config.js
+++ bongbi-api/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "bongbi-api",
       script: "/root/bongbi-SaaS-v2/bongbi-api/.venv/bin/python",
-      args: ["-m", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"],
+      args: ["-m","uvicorn","app.main:app","--host","127.0.0.1","--port","8000"],
       cwd: "/root/bongbi-SaaS-v2/bongbi-api",
       env: {
         VITE_API_BASE_URL: "/api/v1/",
